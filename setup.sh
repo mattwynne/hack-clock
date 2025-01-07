fd8a81457412f1989bdac5b27d197f6448c92945
Matt Wynne <matt@mattwynne.net>
2025-01-06 21:52:18 -0800

    Write rc.local during setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-sudo apt-get install python-dev-is-python3 --yes
+sudo apt-get install -y python3-smbus
+sudo apt-get install -y i2c-tools
+sudo apt-get install -y python-dev-is-python3
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+cat > /etc/rc.local<< EOF 
+#!/bin/sh -e
+
+sudo /home/pi/hack-clock/run_server.sh &
+
+exit 0
+EOF
